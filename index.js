6ad5323901048747729c50fabfcab913f9023d18
R-StevenCastillo <steven14.castillo14@gmail.com>
2024-01-18 15:12:49 -0600

    Implemented POST endpoint '/login' for user authentication

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
 require('dotenv').config();
 const mongoURI = process.env.MONGODB_URI;
+const secretString = process.env.SECRET_STRING;
 const User = require('./user');
 
 mongoose.connect(mongoURI);
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json());
+const signToken = _id => jwt.sign({ _id } , secretString);
 
 app.post('/register', async (req, res) => {
     const { body } = req
@@ -23,8 +25,30 @@ app.post('/register', async (req, res) => {
         const salt = await bcrypt.genSalt();
         const hashed = await bcrypt.hash(body.password, salt);
         const user = await User.create({ email: body.email, password: hashed, salt });
+        const signed = signToken(user.id);
+        res.status(201).send(signed);
     } catch (err) {
         console.log(err);
+        res.status(500).send(err.message);
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const { body } = req
+    try {
+        const user = await User.findOne({ email: body.email });
+        if (!user) {
+            res.status(403).send('usuario y/o contraseña inválida');
+        } else {
+            const isMatch = await bcrypt.compare(body.password, user.password);
+            if (isMatch) {
+                const signed = signToken(user._id);
+                res.status(200).send(signed);
+            } else {
+                res.status(403).send('usuario y/o contraseña inválida');
+            }
+        }
+    } catch (err) {
         res.status(500).send(err.message);
     }
 })
